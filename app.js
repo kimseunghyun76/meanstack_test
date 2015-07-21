45b235412d7fd1bdf4d3cff9ac7f4576434ac6d6
kimseunghyun <kimseunghyun76@gmail.com>
2015-07-21 19:34:34 +0900

    today, it is cloudy

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
+
+mongoose.connect('mongodb://localhost/news');
+
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -58,3 +65,5 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
+
+
